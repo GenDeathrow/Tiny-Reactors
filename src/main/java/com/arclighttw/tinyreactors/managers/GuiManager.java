246902e8dfd3545afd5f9b6a550053ea3c42375f
Tom <tomawright95@gmail.com>
2017-10-23 10:05:18 +0100

    Add Energy Port gui

--- src/main/java/com/arclighttw/tinyreactors/managers/GuiManager.java
+++ src/main/java/com/arclighttw/tinyreactors/managers/GuiManager.java
@@ -1,8 +1,11 @@
 package com.arclighttw.tinyreactors.managers;
 
 import com.arclighttw.tinyreactors.client.gui.GuiReactorController;
+import com.arclighttw.tinyreactors.client.gui.GuiReactorEnergyPort;
 import com.arclighttw.tinyreactors.container.ContainerReactorController;
+import com.arclighttw.tinyreactors.container.ContainerReactorEnergyPort;
 import com.arclighttw.tinyreactors.tiles.TileEntityReactorController;
+import com.arclighttw.tinyreactors.tiles.TileEntityReactorEnergyPort;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
@@ -13,6 +16,7 @@ import net.minecraftforge.fml.common.network.IGuiHandler;
 public class GuiManager implements IGuiHandler
 {
 	public static final int REACTOR_CONTROLLER = 0;
+	public static final int REACTOR_ENERGY_PORT = 1;
 	
 	@Override
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
@@ -26,6 +30,11 @@ public class GuiManager implements IGuiHandler
 				break;
 			
 			return new GuiReactorController(player.inventory, (TileEntityReactorController)tile);
+		case REACTOR_ENERGY_PORT:
+			if(tile == null || !(tile instanceof TileEntityReactorEnergyPort))
+				break;
+			
+			return new GuiReactorEnergyPort(player.inventory, (TileEntityReactorEnergyPort)tile);
 		}
 		
 		return null;
@@ -43,6 +52,11 @@ public class GuiManager implements IGuiHandler
 				break;
 			
 			return new ContainerReactorController(player.inventory, (TileEntityReactorController)tile);
+		case REACTOR_ENERGY_PORT:
+			if(tile == null || !(tile instanceof TileEntityReactorEnergyPort))
+				break;
+			
+			return new ContainerReactorEnergyPort(player.inventory, (TileEntityReactorEnergyPort)tile);
 		}
 		
 		return null;
